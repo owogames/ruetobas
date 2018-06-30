04951484f8be70791a532545e9b200432939ace5
wonrzrzeczny <39885183+wonrzrzeczny@users.noreply.github.com>
2018-06-30 23:14:38 +0200

    Textursy

--- WindowsGame1/WindowsGame1/WindowsGame1/Render.cs
+++ WindowsGame1/WindowsGame1/WindowsGame1/Render.cs
@@ -11,30 +11,30 @@ namespace Ruetobas
     {
         public static Texture2D RenderTunnel(Game game, SpriteBatch spriteBatch, int ID)
         {
-            Texture2D target = new Texture2D(game.GraphicsDevice, 210, 300);
-            Color[] maskData = new Color[210 * 300];
+            Texture2D target = new Texture2D(game.GraphicsDevice, 420, 600);
+            Color[] maskData = new Color[420 * 600];
             Color[] dirtData = new Color[420 * 600];
-            Color[] output = new Color[210 * 300];
+            Color[] output = new Color[420 * 600];
             Logic.cardTexture[ID].GetData(maskData);
             Logic.tileDirt.GetData(dirtData);
-            for (int x = 0; x < 210; x++)
+            for (int x = 0; x < 420; x++)
             {
-                for (int y = 0; y < 300; y++)
+                for (int y = 0; y < 600; y++)
                 {
-                    if (maskData[x + 210 * y].B < 20)
+                    if (maskData[x + 420 * y].B < 20)
                     {
-                        output[x + 210 * y] = dirtData[2 * (x + 420 * y)];
+                        output[x + 420 * y] = dirtData[x + 420 * y];
                         double amount = 0;
-                        for (int x1 = Math.Max(0, x - 12); x1 < Math.Min(210, x + 13); x1++)
-                            for (int y1 = Math.Max(0, y - 12); y1 < Math.Min(300, y + 13); y1++)
-                                if (maskData[x1 + 210 * y1].B > 20)
+                        for (int x1 = Math.Max(0, x - 12); x1 < Math.Min(420, x + 13); x1++)
+                            for (int y1 = Math.Max(0, y - 12); y1 < Math.Min(600, y + 13); y1++)
+                                if (maskData[x1 + 420 * y1].B > 20)
                                     amount += 0.25f;
-                        output[x + 210 * y].R -= Math.Min((byte)amount, output[x + 210 * y].R);
-                        output[x + 210 * y].G -= Math.Min((byte)amount, output[x + 210 * y].G);
-                        output[x + 210 * y].B -= Math.Min((byte)amount, output[x + 210 * y].B);
+                        output[x + 420 * y].R -= Math.Min((byte)amount, output[x + 420 * y].R);
+                        output[x + 420 * y].G -= Math.Min((byte)amount, output[x + 420 * y].G);
+                        output[x + 420 * y].B -= Math.Min((byte)amount, output[x + 420 * y].B);
                     }
                     else
-                        output[x + 210 * y] = Color.Transparent;
+                        output[x + 420 * y] = Color.Transparent;
                 }
             }
 
