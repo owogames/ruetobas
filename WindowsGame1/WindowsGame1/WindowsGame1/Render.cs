5edb78ca54ee52ec60874daf13a6a582cc59ce92
wonrzrzeczny <39885183+wonrzrzeczny@users.noreply.github.com>
2018-08-02 23:30:28 +0200

    Shadow generating speed up
    SUMY PREFIKSOWE

--- WindowsGame1/WindowsGame1/WindowsGame1/Render.cs
+++ WindowsGame1/WindowsGame1/WindowsGame1/Render.cs
@@ -9,8 +9,6 @@ namespace Ruetobas
 {
     public static class Render
     {
-        public const bool RENDERSHADOWS = true;
-
         public static Texture2D RenderTunnel(Game game, SpriteBatch spriteBatch, int ID)
         {
             Console.WriteLine("Rendering texture ID: {0}", ID);
@@ -18,6 +16,7 @@ namespace Ruetobas
             Color[] maskData = new Color[420 * 600];
             Color[] dirtData = new Color[420 * 600];
             Color[] output = new Color[420 * 600];
+            float[,] prefixSums = new float[421, 601];
             if (ID > 0)
                 Logic.cardTexture[ID].GetData(maskData);
             else
@@ -27,23 +26,24 @@ namespace Ruetobas
             }
             Logic.tileDirt.GetData(dirtData);
             for (int x = 0; x < 420; x++)
+                for (int y = 0; y < 600; y++)
+                {
+                    prefixSums[x + 1, y + 1] = prefixSums[x, y + 1] + prefixSums[x + 1, y] - prefixSums[x, y];
+                    if (maskData[x + 420 * y].B > 20)
+                        prefixSums[x + 1, y + 1] += 0.25f;
+                }
+
+            for (int x = 0; x < 420; x++)
             {
                 for (int y = 0; y < 600; y++)
                 {
                     if (maskData[x + 420 * y].B < 20)
                     {
                         output[x + 420 * y] = dirtData[x + 420 * y];
-                        if (RENDERSHADOWS)
-                        {
-                            double amount = 0;
-                            for (int x1 = Math.Max(0, x - 12); x1 < Math.Min(420, x + 13); x1++)
-                                for (int y1 = Math.Max(0, y - 12); y1 < Math.Min(600, y + 13); y1++)
-                                    if (maskData[x1 + 420 * y1].B > 20)
-                                        amount += 0.25f;
-                            output[x + 420 * y].R -= Math.Min((byte)amount, output[x + 420 * y].R);
-                            output[x + 420 * y].G -= Math.Min((byte)amount, output[x + 420 * y].G);
-                            output[x + 420 * y].B -= Math.Min((byte)amount, output[x + 420 * y].B);
-                        }
+                        double amount = prefixSums[Math.Min(420, x + 13), Math.Min(600, y + 13)] - prefixSums[Math.Max(0, x - 12), Math.Min(600, y + 13)] - prefixSums[Math.Min(420, x + 13), Math.Max(0, y - 12)] + prefixSums[Math.Max(0, x - 12), Math.Max(0, y - 12)];
+                        output[x + 420 * y].R -= Math.Min((byte)amount, output[x + 420 * y].R);
+                        output[x + 420 * y].G -= Math.Min((byte)amount, output[x + 420 * y].G);
+                        output[x + 420 * y].B -= Math.Min((byte)amount, output[x + 420 * y].B);
                     }
                     else
                         output[x + 420 * y] = Color.Transparent;
